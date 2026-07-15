25140ed3688790180961dab3f3cc6a7a468d5ce0
bacha <bachaeshetu@gmail.com>
2026-07-15 15:09:18 +0300

    feat: modularize routes and implement full CRUD operations for subscriptions

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 import express from "express";
+import { PORT } from './config/env.js'
+import userRouter from "./routes/user.routes.js";
+import authRouter from "./routes/auth.routes.js";
+import subscriptionRouter from "./routes/subscription.routes.js";
 
 const app = express();
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/users', userRouter)
+app.use('/api/v1/subscription', subscriptionRouter)
+
 
 app.get('/', (req, res) => {
     res.send('  Wellcome to the subscription Tracker API!')
 })
-app.listen(3000, () => {
-    console.log(`Subscription Tracker API is running on  http://localhost:3000`)
+app.listen(PORT || 3000, () => {
+    console.log(`Subscription Tracker API is running on  http://localhost:${PORT || 3000}`)
 })
 
 export default app;
+// Trigger restart
--- routes/subscription.routes.js
+++ routes/subscription.routes.js
@@ -1,7 +1,29 @@
 import { Router } from "express";
 
 const subscriptionRouter = Router();
-subscriptionRouter.post('/', (req, res) => res.send({ title: 'Get all subscriptions' }));
+subscriptionRouter.get('/', (req, res) => res.send({ title: 'Get all subscriptions' }));
+
+subscriptionRouter.get('/:id', (req, res) => res.send({ title: 'Get subscription details' }));
+
+subscriptionRouter.post('/', (req, res) => res.send({ title: 'Create subscriptions' }));
+
+subscriptionRouter.post('/:id', (req, res) => res.send({ title: 'update subscriptions' }));
+
+subscriptionRouter.delete('/:id', (req, res) => res.send({ title: 'delete subscription' }));
+
+subscriptionRouter.get('/user/:id', (req, res) => res.send({ title: 'Get all users subscriptions' }));
+
+subscriptionRouter.put('/:id/cancel', (req, res) => res.send({ title: 'cancel subscription' }));
+
+
+subscriptionRouter.get('/upcoming-renewals', (req, res) => res.send({ title: 'Get upcoming renewals' }));
+
+
+
+
+
+
+
 
 
 
