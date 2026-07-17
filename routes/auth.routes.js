30e2e83b8722231ec9bc79a72db7ddab79e7da4e
bacha <bachaeshetu@gmail.com>
2026-07-17 07:35:07 +0300

    feat: implement user authentication with JWT and bcrypt and relocate model files

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
+import { signIn, signOut, signUp } from "../controllers/auth.controller";
 
 const authRouter = Router();
-authRouter.post('/sign-up', (req, res) => res.send({ title: 'sign-up' }));
-authRouter.post('/sign-in', (req, res) => res.send({ title: 'sign-in' }));
-authRouter.post('/sign-out', (req, res) => res.send({ title: 'sign-out' }));
+// Path: /api/auth/sign-up, sign-in, sign-out (all POST requests)
+authRouter.post('/sign-up', signUp);
+authRouter.post('/sign-in', signIn);
+authRouter.post('/sign-out', signOut);
 
 export default authRouter;
